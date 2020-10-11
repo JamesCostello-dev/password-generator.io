93e631a5a872804be952f3f5f66dcdb4d3227454
James Costello <jcotab@gmail.com>
2020-10-10 23:53:36 -0700

    final commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbers = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']; 
-var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(',')'];
+var numbers = ['0','1','2','3','4','5','6','7','8','9']; 
+var symbols = ['!','@','#','$','%','^','&','*','(',')'];
 
 var generate = '';
 var generateIndex = [];
@@ -81,7 +81,6 @@ if (allParameters.includes(true)) {
   return '';
 } 
 
-debugger;
 
 if (promptLowerCase) {
     var lowerCaseIndex = Math.floor(Math.random() * lowerCase.length);
